da7fd8c0a886c1e60541c651d7499a18087c26a5
steppinlo <lo.stephaniec@gmail.com>
2016-12-07 16:32:35 -0500

    Initial Commit

---
+++ SearchBarWithTags/SearchBarWithTags.h
@@ -0,0 +1,19 @@
+//
+//  SearchBarWithTags.h
+//  SearchBarWithTags
+//
+//  Created by Stephanie Lo on 12/7/16.
+//  Copyright © 2016 Stephanie Lo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SearchBarWithTags.
+FOUNDATION_EXPORT double SearchBarWithTagsVersionNumber;
+
+//! Project version string for SearchBarWithTags.
+FOUNDATION_EXPORT const unsigned char SearchBarWithTagsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchBarWithTags/PublicHeader.h>
+
+
